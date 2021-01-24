42f2e79e1bfd040911aa35c495998f7b5af6a3dc
Rony <levy95rony@gmail.com>
2021-01-24 22:18:35 +0200

    final commit

--- cfg/dist.js
+++ cfg/dist.js
@@ -10,7 +10,6 @@ let defaultSettings = require('./defaults');
 let BowerWebpackPlugin = require('bower-webpack-plugin');
 
 let config = Object.assign({}, baseConfig, {
-<<<<<<< HEAD
     entry: path.join(__dirname, '../src/index'),
     cache: false,
     devtool: 'sourcemap',
@@ -28,30 +27,10 @@ let config = Object.assign({}, baseConfig, {
         new webpack.NoErrorsPlugin()
     ],
     module: defaultSettings.getDefaultModules()
-=======
-  entry: path.join(__dirname, '../src/index'),
-  cache: false,
-  devtool: 'sourcemap',
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    }),
-    new BowerWebpackPlugin({
-      searchResolveModulesDirectories: true
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: defaultSettings.getDefaultModules()
->>>>>>> 54da43cda7bbcc34d8350a5d7ef65d904bc75acb
 });
 
 // Add needed loaders to the defaults here
 config.module.loaders.push({
-<<<<<<< HEAD
     test: /\.(js|jsx)$/,
     loader: 'babel',
     include: [].concat(
@@ -60,14 +39,3 @@ config.module.loaders.push({
 });
 
 module.exports = config;
-=======
-  test: /\.(js|jsx)$/,
-  loader: 'babel',
-  include: [].concat(
-    config.additionalPaths,
-    [ path.join(__dirname, '/../src') ]
-  )
-});
-
-module.exports = config;
->>>>>>> 54da43cda7bbcc34d8350a5d7ef65d904bc75acb
